8330e6738f4f822a913226cc78a8d0bf2ce14db9
Helen Chong <119173961+helenclx@users.noreply.github.com>
2023-06-03 23:35:38 +0800

    Re-added interactive rating popup
    Use parseInt to convert button options to numbers

--- interactive-rating-component/index.js
+++ interactive-rating-component/index.js
@@ -4,19 +4,21 @@ const ratingOptions = document.querySelectorAll("input[name=rating] + label");
 const submitBtnEl = document.querySelector(".rating-card__submit-btn");
 const userRatingEl = document.querySelector("#user-rating");
 
+let rating = 0;
+
+ratingOptions.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        const selectedRating = e.target.textContent;
+        rating = parseInt(selectedRating);
+    })
+})
+
 submitBtnEl.addEventListener('click', () => {
-    // if (rating === 0) {
-    //     alert("Please select your rating!");
-    // } else {
-    //     ratingCardEl.style.display = "none";
-    //     thankyouCardEl.style.display = "flex";
-    //     rating = document.querySelector("input[name=rating]:checked").value;
-    //     userRatingEl.textContent = rating;
-    // }
-    let rating = 0;
-    ratingCardEl.style.display = "none";
-    thankyouCardEl.style.display = "flex";
-    rating = document.querySelector("input[name=rating]:checked").value;
-    userRatingEl.textContent = rating;
-    console.log(rating);
+    if (rating === 0) {
+        alert("Please select your rating!");
+    } else {
+        ratingCardEl.style.display = "none";
+        thankyouCardEl.style.display = "flex";
+        userRatingEl.textContent = rating;
+    }
 });
