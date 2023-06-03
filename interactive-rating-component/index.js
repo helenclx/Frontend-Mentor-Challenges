0f53a7711f9879e8b3299d39179bfb047affbb3c
Helen Chong <119173961+helenclx@users.noreply.github.com>
2023-06-03 23:26:06 +0800

    Revert popup change

--- interactive-rating-component/index.js
+++ interactive-rating-component/index.js
@@ -4,22 +4,19 @@ const ratingOptions = document.querySelectorAll("input[name=rating] + label");
 const submitBtnEl = document.querySelector(".rating-card__submit-btn");
 const userRatingEl = document.querySelector("#user-rating");
 
-let rating = 0;
-
-ratingOptions.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-        const selectedRating = e.target.textContent;
-        rating = selectedRating;
-        console.log(rating);
-    })
-})
-
 submitBtnEl.addEventListener('click', () => {
-    if (rating === 0) {
-        alert("Please select your rating!");
-    } else {
-        ratingCardEl.style.display = "none";
-        thankyouCardEl.style.display = "flex";
-        userRatingEl.textContent = rating;
-    }
+    // if (rating === 0) {
+    //     alert("Please select your rating!");
+    // } else {
+    //     ratingCardEl.style.display = "none";
+    //     thankyouCardEl.style.display = "flex";
+    //     rating = document.querySelector("input[name=rating]:checked").value;
+    //     userRatingEl.textContent = rating;
+    // }
+    let rating = 0;
+    ratingCardEl.style.display = "none";
+    thankyouCardEl.style.display = "flex";
+    rating = document.querySelector("input[name=rating]:checked").value;
+    userRatingEl.textContent = rating;
+    console.log(rating);
 });
