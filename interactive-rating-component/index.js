0d7dba49b4ec74823b127d55d8f173a6abc271b7
Helen Chong <119173961+helenclx@users.noreply.github.com>
2023-06-03 17:44:01 +0800

    Script hotfix
    Temporary fix to the issue of preventing user from submitting even after selecting a rating.

--- interactive-rating-component/index.js
+++ interactive-rating-component/index.js
@@ -4,15 +4,19 @@ const ratingOptions = document.querySelectorAll("input[name=rating] + label");
 const submitBtnEl = document.querySelector(".rating-card__submit-btn");
 const userRatingEl = document.querySelector("#user-rating");
 
-let rating = 0;
-
 submitBtnEl.addEventListener('click', () => {
-    if (rating === 0) {
-        alert("Please select your rating!");
-    } else {
-        ratingCardEl.style.display = "none";
-        thankyouCardEl.style.display = "flex";
-        rating = document.querySelector("input[name=rating]:checked").value;
-        userRatingEl.textContent = rating;
-    }
+    // if (rating === 0) {
+    //     alert("Please select your rating!");
+    // } else {
+    //     ratingCardEl.style.display = "none";
+    //     thankyouCardEl.style.display = "flex";
+    //     rating = document.querySelector("input[name=rating]:checked").value;
+    //     userRatingEl.textContent = rating;
+    // }
+    let rating = 0;
+    ratingCardEl.style.display = "none";
+    thankyouCardEl.style.display = "flex";
+    rating = document.querySelector("input[name=rating]:checked").value;
+    userRatingEl.textContent = rating;
+    console.log(rating);
 });
