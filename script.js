30b0cbaac5b637946a90dcbab0f9be41f763c486
Helen Chong <119173961+helenclx@users.noreply.github.com>
2024-03-24 18:08:58 +0800

    Add button to scroll to top

--- script.js
+++ script.js
@@ -20,3 +20,21 @@ const displaySolutions = (arr) => {
     });
 };
 displaySolutions(solutions);
+
+const topButton = document.querySelector(".top-btn");
+
+window.onscroll = () => {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topButton.classList.remove("hidden");
+    } else {
+        topButton.classList.add("hidden");
+    }
+}
+
+topButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+});
